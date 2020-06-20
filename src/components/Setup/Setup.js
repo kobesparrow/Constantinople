fb1f09b2095e9ae168e85c4fa69ddf776fa4873e
Mark R. Collins <markcollins.md@gmail.com>
2020-06-20 17:19:46 -0600

    able to add expansions

--- src/components/Setup/Setup.js
+++ src/components/Setup/Setup.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react';
 
 class Setup extends Component {
+  constructor() {
+    super()
 
+    this.state = {
+      mocha: false,
+      letters: false,
+      grandBazaar: false
+    }
+  }
 
   generateBoard = () => {
-    // let tiles = [1,2,3,4,5,6,8,9,10,11,12,13,14,15,16];
-
     let tiles = [' Wainwright', 'Fabric Warehouse', 'Spice Warehouse', 'Fruit Warehouse', 'Post Office', 'Caravansary', 'Black Market', 'Tea House', 'Large Market', 'Small Market', 'Police Station', `Sultan's Palace`, 'Small Mosque', 'Great Mosque', 'Gemstone Dealer']
 
+    tiles = this.addExpansions(tiles)
+
+    
     let sortedTiles = tiles.sort(() => 0.5 - Math.random());
     this.addFountain(sortedTiles)
+  }
+
+
+  addExpansions = (tiles) => {
+    if (this.state.mocha && this.state.letters) {
+      tiles = tiles.concat('Catacombs')
+      this.setState({ grandBazaar: true })
+    }
+
+
+    if (this.state.mocha) {
+      tiles = tiles.concat(['Roasting Plant', 'Tavern', 'Coffee House', 'Guild Hall'])
+    }
+
+    if (this.state.letters) {
+      tiles = tiles.concat(['Kiosk', 'Auction House', 'Secret Society', 'Embassy'])
+    }
+
+    return tiles
   }
 
   addFountain = (sortedTiles) => {
@@ -20,23 +48,22 @@ class Setup extends Component {
     this.props.setBoardLayout(sortedTiles)
   }
 
-  // checkFountain = (sortedTiles) => {
-  //   let fountain = 7
-  //   let fountainLocation = sortedTiles.findIndex(value => value === 7)
-  //   console.log(fountainLocation)
-  //   if (fountainLocation === (6 || 7 || 10 || 11)) {
-  //     console.log('not an appropriate board');
-  //     // this.generateBoard();
-
-
-
-  //   } else {
-  //     console.log('test is an appropriate board')
-  //   }
-  // }
+  adjustExpansionSelection = (event) => {
+    this.setState({ [event.target.id]: !this.state[event.target.id] })
+  }
 
   render() {
     return <div>
+        <section>
+          <p>Add Expansions:</p>
+          <form>
+            <input type="checkbox" id="mocha" name="expansion" onClick={ this.adjustExpansionSelection } />
+            <label for="mocha">Mocha and Baksheesh</label>
+            <input type="checkbox" id="letters" name="expansion" onClick={ this.adjustExpansionSelection } />
+            <label for="letters">Seals and Letters</label>
+            
+          </form>
+        </section>
         <button onClick={ this.generateBoard }>Produce game board</button>
       </div>
   }
